bc3d72d16ca5aaa0260e53483ea74248ce9092c8
Segun Awe <Segunawet@gmail.com>
2023-03-28 20:18:06 -0400

    remove unecessary lines in appjs

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Financial Budgeting App</h1>
-        <Header/>
-        {/* Add any additional header components here */}
-      </header>
-      <main>
-        {/* Add your budgeting app components here */}
-      </main>
+      <Header />
+      <h1>Financial Budgeting App</h1>
     </div>
   );
 }
