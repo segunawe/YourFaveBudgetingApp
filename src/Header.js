5cdb324719179c9390b5745958a78b4816a42e8b
Segun Awe <Segunawet@gmail.com>
2023-03-28 20:15:21 -0400

    adding app changes

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Toolbar, IconButton, Box, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Box, Typography } from '@mui/material';
 import MenuIcon  from '@mui/icons-material/Menu';
 
 function Header() {
