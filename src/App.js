ae62dca5cc911855aa7d17fe58ef8532fd08c97c
Segun Awe <Segunawet@gmail.com>
2023-04-08 23:07:28 -0400

    container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Header';
 import CircleRow from './Bucket';
+import Container from './Container';
 
 function App() {
   return (
     <>
       <Header/>
       <CircleRow />
+      <Container />
     </>
   );
 };
